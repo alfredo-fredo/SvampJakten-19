10a2819522f3730b984fe0a3aef44fa4394ad0ee
HenrikDore <henrikdore@gmail.com>
2019-11-11 13:43:01 +0100

    google maps test commit

---
+++ app/src/main/java/com/example/googlemapstest/MapsActivity.java
@@ -0,0 +1,84 @@
+package com.example.googlemapstest;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 2);
+        setContentView(R.layout.activity_maps);
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        float zoomLevel = (float) 18.0;
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+
+            System.out.println("Något gick fel");
+        }
+
+        // Add a marker in Göteborg and move the camera
+        LatLng göteborg = new LatLng(57.70, 11.97);
+        mMap.getUiSettings().setZoomControlsEnabled(true); //zoom + / - button
+        mMap.addMarker(new MarkerOptions().position(göteborg).title("YOUR POSITION"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(göteborg, zoomLevel));
+
+
+        // mGeoDataClient = Places.getGeoDataClient(this, null);
+
+
+        //fixa utzoomning
+        //fixa om man kan lägga ut egna pins
+        //filtrera bort alternativ
+
+
+
+
+    }
+
+
+
+}
