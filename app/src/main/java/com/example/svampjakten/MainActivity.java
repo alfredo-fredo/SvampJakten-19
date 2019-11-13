9c12b4621a334fd97470568c1e97c2ec18574e12
HenrikDore <henrikdore@gmail.com>
2019-11-13 14:22:55 +0100

    Merged Alfreds design to my google maps code.

--- app/src/main/java/com/example/svampjakten/MainActivity.java
+++ app/src/main/java/com/example/svampjakten/MainActivity.java
@@ -3,22 +3,39 @@ package com.example.svampjakten;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     Toolbar tl;
     ImageButton imgLeft, imgRight;
     DrawerLayout mDrawerLayout;
     ActionBarDrawerToggle mToggle;
     View leftDrawer, rightDrawer;
+    private GoogleMap mMap;
 
     boolean mustLogin = true;
 
@@ -26,6 +43,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 2);
+
+
+        //
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        Log.d("myTag", "Loglog looog.");
+            mapFragment.getMapAsync(this);
+            Log.d("myTag", "Maps was not null, map async..");
+
+
+
         tl = findViewById(R.id.toolbar);
         setSupportActionBar(tl);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
@@ -76,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
     void popLogin(){
         View loginView = findViewById(R.id.include_login);
         loginView.setVisibility(View.VISIBLE);
@@ -85,4 +119,54 @@ public class MainActivity extends AppCompatActivity {
         loginView.startAnimation(anim1);
         loginView.startAnimation(anim2);
     }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        Log.d("myTag", "OnMapReady executed.");
+        float zoomLevel = (float) 16.0;
+
+        /**
+         * Asks users for location permission
+         */
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+        Log.d("myTag", "permission denied");
+        }
+
+        /**
+         * Getting the user current location
+         */
+        double longitude = 0.0;
+        double latitude = 0.0;
+
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            else{
+                Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+            }
+        }
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude,longitude), zoomLevel));
+
+
+
+
+
+
+
+
+
+
+    }
+
+
 }
