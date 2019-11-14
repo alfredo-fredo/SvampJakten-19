7ff41743f245755658bf6bd91c4b5e500b083f91
william.v <xiiav@hotmail.se>
2019-11-14 11:23:48 +0100

    lite ändringar Test

--- app/src/main/java/com/example/svampjakten/LoginFragment.java
+++ app/src/main/java/com/example/svampjakten/LoginFragment.java
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -15,9 +17,27 @@ import android.view.ViewGroup;
  */
 public class LoginFragment extends Fragment {
 
+
+    EditText LoginMail;
+    EditText LoginPass;
+    Button Login;
+    Button LoginFacebook;
+    Button Register;
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
+
+        LoginMail = getActivity().findViewById(R.id.Signin_Email);
+        LoginPass = getActivity().findViewById(R.id.Signin_Password);
+        Login = getActivity().findViewById(R.id.Button_Login);
+        LoginFacebook = getActivity().findViewById(R.id.Button_LoginFACEBOOK);
+        Register = getActivity().findViewById(R.id.Button_NewAccount);
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
     }
