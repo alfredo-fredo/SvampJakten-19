5974b2a98d3645ec751f3dec115c6a7fb3751863
antonlindgren <anton_lindgren@live.se>
2019-12-05 01:07:41 +0100

    Now you can take picture and the image saves in a folder at Firebase The same goes for pictrues from the gallery /Anton

--- app/src/main/java/com/example/svampjakten/CreatePinFragment.java
+++ app/src/main/java/com/example/svampjakten/CreatePinFragment.java
@@ -20,15 +20,19 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+
+import java.io.ByteArrayOutputStream;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -46,14 +50,16 @@ public class CreatePinFragment extends Fragment {
     static final int REQUEST_TAKE_PHOTO = 1;
 
     private Button galleryBtn;
+    private Button takePhotoBtn;
 
 
-
-
+    private static final int TAKE_IMAGE_CODE =27;
     private static final int CAMERA_REQUEST_CODE = 1;
     private static final int GALLERY_INTENT = 2;
     private Uri mImageUri = null;
     private StorageReference mStorage;
+    private StorageReference storage;
+    private String key;
 
 
     @Override
@@ -69,7 +75,21 @@ public class CreatePinFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
 
+
+        storage = FirebaseStorage.getInstance().getReference();
         mStorage = FirebaseStorage.getInstance().getReference();
+
+        takePhotoBtn = (Button) getView().findViewById(R.id.create_pin_add_photo);
+        takePhotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAMERA_REQUEST_CODE);
+            }
+        });
+
+
+
 
         galleryBtn = (Button) getView().findViewById(R.id.gallery_btn);
 
@@ -79,10 +99,11 @@ public class CreatePinFragment extends Fragment {
                 Intent intent = new Intent(Intent.ACTION_PICK);
 
                 intent.setType("image/*");
-                startActivityForResult(intent, GALLERY_INTENT);
+                startActivityForResult(intent,GALLERY_INTENT);
 
             }
         });
+
 
 
 
@@ -123,9 +144,10 @@ public class CreatePinFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                //Uri fileUri =
+
                 if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
                     startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+
                 }
             }
         });
@@ -146,18 +168,88 @@ public class CreatePinFragment extends Fragment {
         if (requestCode == GALLERY_INTENT) {
             Log.d("Hej", "Brap brap");
             if (resultCode == RESULT_OK) {
-               Uri uri = data.getData();
-               StorageReference filepath = mStorage.child("Photos").child(uri.getLastPathSegment());
+                Uri uri = data.getData();
+                StorageReference filepath = mStorage.child("kamerabilder").child(uri.getLastPathSegment());
+                DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
 
-               filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                   @Override
-                   public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                       //Toast.makeText(CreatePinFragment.this,"Upload done", Toast.LENGTH_LONG).show();
-                   }
-               });
+                filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(getContext(),"Picture uploaded!", Toast.LENGTH_LONG).show();
+                    }
+                });
 
             }
+
         }
+        if(requestCode == CAMERA_REQUEST_CODE){
+            if(resultCode == RESULT_OK) {
+                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                handleUpload(bitmap);
+            }
+        }
+    }
+    private void handleUpload(Bitmap bitmap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100, baos);
+
+        //String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        final StorageReference reference = FirebaseStorage.getInstance().getReference().child("kamerabilder").child(".jpeg");
+
+        //Uri uri = data.getData();
+        //StorageReference filepath = reference.child("ProfileImage").child(uri.getLastPathSegment());
+        reference.putBytes(baos.toByteArray()).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            /* filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                 @Override
+                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                     getDownloadUrl(reference);
+                 }
+             });
+         }*/
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                getDownloadUrl(reference);
+                Toast.makeText(getContext(),"Picture uploaded!", Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getContext(),"Upload failed!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+    private void getDownloadUrl (StorageReference reference){
+        reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Log.d("Anton", "onSuccess: " + uri);
+                setUserProfileUrl(uri);
+            }
+        });
+
+    }
+
+    private void setUserProfileUrl (Uri uri){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        UserProfileChangeRequest request = new UserProfileChangeRequest.Builder().setPhotoUri(uri).build();
+
+        user.updateProfile(request).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(getContext(), "Profile image uploaded", Toast.LENGTH_SHORT);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getContext(), "Profile image failed...", Toast.LENGTH_SHORT);
+            }
+        });
+    }
+    private void createPin(){
+
     }
 
 
